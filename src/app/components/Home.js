9743adc54430a401524f439cdfd718c4896918f8
Nicholas Suh <nicksuh17@gmail.com>
2024-09-10 21:43:07 -0400

    fix images

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -21,6 +21,7 @@ import { fadeFromLeft, staggerContainer, staggerContainer2 } from "../utils/anim
 export default function Home({ pageState }) {
 
     const images = [
+        jax.src,
         CE.src,
         DDV.src,
         GTReviews.src,
@@ -29,7 +30,13 @@ export default function Home({ pageState }) {
         AA.src,
         Therapal.src,
     ]
-    const [image, setImage] = useState(jax.src);
+    const imageAnimators = useRef(images.map(() => useAnimate()));
+    
+
+    
+
+
+    const [curImageIndex, setCurImageIndex] = useState(0);
     const [imageContainerScope, imageContainerAnimate] = useAnimate();
     const [transitioning, setTransitioning] = useState(false);
     
@@ -39,55 +46,75 @@ export default function Home({ pageState }) {
     const [frameScope, frameAnimate] = useAnimate();
     const [innerFrameScope, innerFrameAnimate] = useAnimate();
 
-    const handleMouseEnter = async (imagesrc) => {
-        console.log(imagesrc);
-        if(imagesrc == image)
+    const animateImage = async (index) => {
+        if(index == curImageIndex)
             return;
-        await imageContainerAnimate(imageContainerScope.current, {
+        const animator = imageAnimators.current[index];
+        
+        const curAnimator = imageAnimators.current[curImageIndex];
+        const options = {
+            duration: 0.3,
+            ease: "easeOut",
+            delay: 0,
+        }
+        const currentOpacity = animator[0].current.style.opacity;
+        
+        curAnimator[1](curAnimator[0].current, {
             opacity: [1, 0],
-        }, {
-            duration: 0.3,
-            ease: "easeOut",
-            delay: 0,
-        });
-        await setImage(imagesrc);
-        await imageContainerAnimate(imageContainerScope.current, {
+        }, options);
+        animator[1](animator[0].current, {
             opacity: [0, 1],
-        }, {
-            duration: 0.3,
-            ease: "easeOut",
-            delay: 0,
-        });
+        }, options);
+        setCurImageIndex(index);
     }
-    const handleMouseLeave = async () => {
-        if(image == jax.src)
-            return;
-        await imageContainerAnimate(imageContainerScope.current, {
-            opacity: [1, 0],
-        }, {
-            duration: 0.3,
-            ease: "easeOut",
-            delay: 0,
-        });
-        setImage(jax.src);
-        await imageContainerAnimate(imageContainerScope.current, {
-            opacity: [0, 1],
-        }, {
-            duration: 0.3,
-            ease: "easeOut",
-            delay: 0,
-        });
-    }
-    
+    // const handleMouseEnter = async (imagesrc) => {
+    //     console.log(imagesrc);
+    //     if(imagesrc == image)
+    //         return;
+    //     await imageContainerAnimate(imageContainerScope.current, {
+    //         opacity: [1, 0],
+    //     }, {
+    //         duration: 0.3,
+    //         ease: "easeOut",
+    //         delay: 0,
+    //     });
+    //     await setImage(imagesrc);
+    //     await imageContainerAnimate(imageContainerScope.current, {
+    //         opacity: [0, 1],
+    //     }, {
+    //         duration: 0.3,
+    //         ease: "easeOut",
+    //         delay: 0,
+    //     });
+    // }
+    // const handleMouseLeave = async () => {
+    //     if(image == jax.src)
+    //         return;
+    //     await imageContainerAnimate(imageContainerScope.current, {
+    //         opacity: [1, 0],
+    //     }, {
+    //         duration: 0.3,
+    //         ease: "easeOut",
+    //         delay: 0,
+    //     });
+    //     setImage(jax.src);
+    //     await imageContainerAnimate(imageContainerScope.current, {
+    //         opacity: [0, 1],
+    //     }, {
+    //         duration: 0.3,
+    //         ease: "easeOut",
+    //         delay: 0,
+    //     });
+    // }
     useEffect(() => {
+        
         if (textRef.current) {
             setTextHeight(textRef.current.clientHeight);
         }
-        images.forEach((image) => {
-            const img = new Image();
-            img.src = image;
-        });
+        
+
     }, []);
+    
     useEffect(() => {
         if (scope.current)
         {
@@ -115,7 +142,7 @@ export default function Home({ pageState }) {
         var currentBorderRadius = "vw";
         if (frameScope) {
             currentBorderRadius = frameScope.current.style.borderRadius;
-            console.log(currentBorderRadius);
+            
         }
         const currentOpacity = frameScope.current.style.opacity;
         if(pageState == "Projects") {
@@ -132,12 +159,24 @@ export default function Home({ pageState }) {
                 
                 borderRadius: [currentBorderRadius, "2vw"],
             }, frameOptions);
-            innerFrameAnimate(innerFrameScope.current, {
-                height: [innerFrameScope.current.clientHeight, "30vw"],
-                width: [innerFrameScope.current.clientWidth, "30vw"],
+            for(let i = 0; i < imageAnimators.current.length; i++) {
                 
-                borderRadius: [currentBorderRadius , "2vw"],
-            }, frameOptions);
+                const animator = imageAnimators.current[i];
+                const height = animator[0].current.clientHeight;
+                const width = animator[0].current.clientWidth;
+                animator[1](animator[0].current, {
+                    height: [height, "30vw"],
+                    width: [width, "30vw"],
+                    borderRadius: [currentBorderRadius, "2vw"],
+                }, frameOptions);
+            }
+
+            // innerFrameAnimate(innerFrameScope.current, {
+            //     height: [innerFrameScope.current.clientHeight, "30vw"],
+            //     width: [innerFrameScope.current.clientWidth, "30vw"],
+                
+            //     borderRadius: [currentBorderRadius , "2vw"],
+            // }, frameOptions);
         } else {
             
             await new Promise(resolve => setTimeout(resolve, 500));
@@ -162,17 +201,28 @@ export default function Home({ pageState }) {
                 width: [frameScope.current.clientWidth, "25vw"],
                 borderRadius: [currentBorderRadius, "17vw"],
             }, frameOptions);
-            await innerFrameAnimate(innerFrameScope.current, {
-                height: [innerFrameScope.current.clientHeight, "25vw"],
-                width: [innerFrameScope.current.clientWidth, "25vw"],
-                borderRadius: [currentBorderRadius, "17vw"],
-            }, frameOptions);
-            handleMouseLeave();
+
+            for(let i = 0; i < imageAnimators.current.length; i++) {
+                const animator = imageAnimators.current[i];
+                
+                const height = animator[0].current.clientHeight;
+                const width = animator[0].current.clientWidth;
+                animator[1](animator[0].current, {
+                    height: [height, "25vw"],
+                    width: [width, "25vw"],
+                    borderRadius: [currentBorderRadius, "17vw"],
+                }, frameOptions);
+            }
+
+            // await innerFrameAnimate(innerFrameScope.current, {
+            //     height: [innerFrameScope.current.clientHeight, "25vw"],
+            //     width: [innerFrameScope.current.clientWidth, "25vw"],
+            //     borderRadius: [currentBorderRadius, "17vw"],
+            // }, frameOptions);
+            // handleMouseLeave();
+            animateImage(0);
         }
-        
-        
-        
-    }
+    }   
     
     
     const imageVariant = {
@@ -195,18 +245,32 @@ export default function Home({ pageState }) {
             
             <div ref={frameScope} className="relative z-30 h-[25vw] w-[25vw] bg-black rounded-full "> 
                 <AnimatePresence >
-                    <motion.div ref={imageContainerScope}>
-                        <motion.img alt="image" 
+
+                    {/* <motion.div ref={imageContainerScope}>
+                        <motion.img alt="image" id="previewImage"
                         priority 
-                        key={image} 
+                        
                         ref={innerFrameScope} 
-                        src={image} 
+                        src={image}
                         initial="hidden"
                         animate="visible"
                         exit="exit"
                          layout className=" absolute h-[25vw] w-[25vw]  bg-blue-100 rounded-full object-cover " />
                         
-                    </motion.div>
+                    </motion.div> */}
+                    {imageAnimators.current.map((animator, index) => {
+                        
+                        
+                        return(
+                            <motion.img alt="image" 
+                            key={index}
+                            
+                            ref={animator[0]}
+                            src={images[index]}
+                            className=" absolute h-[25vw] w-[25vw] opacity-0 bg-blue-100 rounded-full object-cover " 
+                            />
+                        );
+                    })}
 
                 </AnimatePresence>
 
@@ -246,38 +310,38 @@ export default function Home({ pageState }) {
                         key="projects"
                         variants={staggerContainer2}
                     >
-                        <motion.div variants={fadeFromLeft} onMouseEnter={() => setImage(CE.src)} onPointer className=" cursor-pointer " >
+                        <motion.div variants={fadeFromLeft}  onMouseEnter={() => animateImage(1)} onPointer className=" cursor-pointer " >
                             <Project title="Combat Enchantments" description="Combat Enchantments is a Minecraft Mod that adds various enchantments. Made with Java and the Fabric Modloader, Combat Enchantments has garnered over 250k downloads, and has been featured in various Youtube videos." link="https://www.curseforge.com/minecraft/mc-mods/combat-enchantments"
                             dates="August 2021 - Present" technologies="Java, Gradle, Fabric" role="Creator"
                             />
                         </motion.div >
-                        <motion.div variants={fadeFromLeft}  onMouseEnter={() => handleMouseEnter(DDV.src)} className=" cursor-pointer">
+                        <motion.div variants={fadeFromLeft}  onMouseEnter={() => animateImage(2)} className=" cursor-pointer">
                             <Project title="Dungeon Deja Vu" description="Made for Bevy Game jam 5, Dungeon Deja Vu is a platformer with a circular twist. By community vote, DDV placed 8th out of 77 entries, 3rd in game design. DDV was built with Rust and the Bevy game engine, and will (hopefully) be on Steam soon." link="https://dsfhdshdjtsb.itch.io/dungeon-deja-vu"
                             dates="August 2024 - Present" technologies="Rust, Bevy" role="Programmer, Artist"
                             />
                         </motion.div>
-                        <motion.div variants={fadeFromLeft} onMouseEnter={() => handleMouseEnter(GTReviews.src)} className=" cursor-pointer">
+                        <motion.div variants={fadeFromLeft} onMouseEnter={() => animateImage(3)} className=" cursor-pointer">
                             <Project title="GT Reviews" description="GT Reviews is a full stack web app that allows Georgia Tech students to review classes, professors, dining halls, and residence halls. GT Reviews was made for GT Web Dev, and features React, Firebase, and a Rust-built web scraper." link="https://buzzwalk.github.io/reviews/"
                             dates="September - December 2023" technologies="React, Chakra UI, Node.js, Firebase, Rust " role="Project Manager"
                             />
                         </motion.div>
-                        <motion.div variants={fadeFromLeft} onMouseEnter={() => handleMouseEnter(ND.src)} className=" cursor-pointer">
+                        <motion.div variants={fadeFromLeft} onMouseEnter={() => animateImage(4)} className=" cursor-pointer">
                             <Project title="Notedrop" description="Notedrop allows users to place pegs and balls on a Pachinko-style board, and listen to the sounds as the balls hit the pegs. Notedrop won Digital Track 1st Place at Horizons 2024, and was built with Rust and Bevy." link="https://devpost.com/software/note-drop"
                             dates="March 2024" technologies="Rust, Bevy" role="Programmer"
                             />
                         </motion.div>
-                        <motion.div variants={fadeFromLeft} onMouseEnter={() => handleMouseEnter(Roadcode.src)} className=" cursor-pointer">
+                        <motion.div variants={fadeFromLeft} onMouseEnter={() => animateImage(5)} className=" cursor-pointer">
                             <Project title="Roadcode" description="Roadcode is a vacation planning web app that algorithmically generates a roadtrip with points of interest along the way. Roadcode took best overall out of 500 people at Freyhacks, an MLH season 2022 hackathon." 
                             link="https://devpost.com/software/road-code"
                             dates="June 2022" technologies="Javascript, Node.js, OpenTripMap, Google Maps" role="Programmer"
                             />
                         </motion.div>
-                        <motion.div variants={fadeFromLeft}  onMouseEnter={() => handleMouseEnter(AA.src)} className=" cursor-pointer">
+                        <motion.div variants={fadeFromLeft}  onMouseEnter={() => animateImage(6)} className=" cursor-pointer">
                             <Project title="Armor Abilities" description="Armor abilities is another Minecraft Mod I made to add activatable abilities to the game, including dashes, teleports, and transforming into anvils. Armor Abilities was made for both Fabric and Forge, and has around 10k downloads" link="https://www.curseforge.com/minecraft/mc-mods/armor-abilities"
                             dates="June 2023" technologies="Java, Gradle, Fabric, Forge" role="Creator"
                             />
                         </motion.div>
-                        <motion.div variants={fadeFromLeft} onMouseEnter={() => handleMouseEnter(Therapal.src)} className=" cursor-pointer">
+                        <motion.div variants={fadeFromLeft} onMouseEnter={() => animateImage(7)} className=" cursor-pointer">
                             <Project title="Therapal" description="Therapal is an omegle-style web app with AI that links together people who struggle with mental disorders. Powered by GPT-3.5, Therapal took 1st place at Hack United 2023, out of 400 people. " link="https://devpost.com/software/note-drop"
                             dates="August 2023" technologies="React, Material UI, GPT-3.5, Firebase, Node.js " role="Programmer"
                             />
